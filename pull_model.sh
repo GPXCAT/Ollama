887cd97158fa7dc8431e627ba3e32b489012042f
John <john.chang@starnic.com.tw>
2024-04-17 13:37:13 +0800

    up

--- pull_model.sh
+++ pull_model.sh
@@ -1,3 +1,4 @@
+# https://ollama.com/library
 docker compose exec ollama ollama pull llama2
 docker compose exec ollama ollama pull mistral
 docker compose exec ollama ollama pull dolphin-phi
@@ -7,9 +8,12 @@ docker compose exec ollama ollama pull starling-lm
 docker compose exec ollama ollama pull codellama
 docker compose exec ollama ollama pull llama2-uncensored
 docker compose exec ollama ollama pull llama2:13b
-docker compose exec ollama ollama pull llama2:70b
+# docker compose exec ollama ollama pull llama2:70b
 docker compose exec ollama ollama pull orca-mini
 docker compose exec ollama ollama pull vicuna
 docker compose exec ollama ollama pull llava
 docker compose exec ollama ollama pull gemma:2b
 docker compose exec ollama ollama pull gemma:7b
+docker compose exec ollama ollama pull wizardlm2
+docker compose exec ollama ollama pull llama2-chinese
+docker compose exec ollama ollama pull openchat
